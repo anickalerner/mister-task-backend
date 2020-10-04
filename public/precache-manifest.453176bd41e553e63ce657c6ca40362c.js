e5c5a616505417902ea04b48b769a1f964442c89
Anna Lerner <anicka.lerner@gmail.com>
2020-10-04 16:23:15 +0300

    Heroku

---
+++ public/precache-manifest.453176bd41e553e63ce657c6ca40362c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b356beb7986f91e16aadfd934b1de72b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "241b41ede39912a14e04",
+    "url": "/static/css/main.63dbaf86.chunk.css"
+  },
+  {
+    "revision": "0937f422ca0bf71797bf",
+    "url": "/static/js/2.344ca034.chunk.js"
+  },
+  {
+    "revision": "85a08104991a260e6b3c376fd8924b35",
+    "url": "/static/js/2.344ca034.chunk.js.LICENSE"
+  },
+  {
+    "revision": "241b41ede39912a14e04",
+    "url": "/static/js/main.e296348d.chunk.js"
+  },
+  {
+    "revision": "c9f1875f7e246a67df7e",
+    "url": "/static/js/runtime-main.cb82405f.js"
+  }
+]);
